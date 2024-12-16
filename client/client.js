89363109a60de96efac08b2eb04eaf77cf461803
wintvue <jjovito33@gmail.com>
2024-12-16 18:30:15 +0700

    modify client socket

--- client/client.js
+++ client/client.js
@@ -1,3 +1,9 @@
 import { initializeGame } from './game.js';
-const websocketUrl = import.meta.env.VITE_WEBSOCKET_URL || 'ws://localhost:8080';
+let websocketUrl;
+try {
+    websocketUrl = import.meta.env.VITE_WEBSOCKET_URL || 'ws://localhost:8080';
+} catch {
+    websocketUrl = 'ws://localhost:8080';
+}
+
 initializeGame(websocketUrl);
